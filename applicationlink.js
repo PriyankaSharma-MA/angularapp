fc845cb2462956f1676d81e6b2717c69b9524d77
priyanka sharma <priyanka.sharma@mediaagility.com>
2019-01-23 23:25:24 +0530

    Update applicationlink.js

--- applicationlink.js
+++ applicationlink.js
@@ -1,4 +1,4 @@
 // JavaScript
 var DashboardsList='da1c7354-20dc-41c2-adfb-ff9f2ca66b9b'
 var DashboardFilter='a91577da-b164-40f6-9b75-c92dfad8b9e7'
-var DefaultLink='6272f3f4-370b-4491-8fe4-52132592f87b'
+var DefaultLink='6272f3f4-370b-4491-8fe4-52132592f
